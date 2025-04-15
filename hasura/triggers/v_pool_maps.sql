c5ff585fb3476468df70fa149ced6031db27bc00
Luke Policinski <luke@lukepolo.com>
2025-04-15 16:25:47 -0400

    chore: move into singular file

--- hasura/triggers/v_pool_maps.sql
+++ hasura/triggers/v_pool_maps.sql
@@ -10,3 +10,33 @@ $$;
 
 DROP TRIGGER IF EXISTS ti_v_pool_maps ON public.v_pool_maps;
 CREATE TRIGGER ti_v_pool_maps INSTEAD OF INSERT ON public.v_pool_maps FOR EACH ROW EXECUTE FUNCTION public.ti_v_pool_maps();
+
+CREATE OR REPLACE FUNCTION public.tu_v_pool_maps() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE public._map_pool
+  SET map_pool_id = NEW.map_pool_id,
+      map_id = NEW.id
+  WHERE map_pool_id = OLD.map_pool_id AND map_id = OLD.id;
+  
+  RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS tu_v_pool_maps ON public.v_pool_maps;
+CREATE TRIGGER tu_v_pool_maps INSTEAD OF UPDATE ON public.v_pool_maps FOR EACH ROW EXECUTE FUNCTION public.tu_v_pool_maps();
+
+CREATE OR REPLACE FUNCTION public.td_v_pool_maps() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  DELETE FROM public._map_pool
+  WHERE map_pool_id = OLD.map_pool_id AND map_id = OLD.id;
+  
+  RETURN OLD;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS td_v_pool_maps ON public.v_pool_maps;
+CREATE TRIGGER td_v_pool_maps INSTEAD OF DELETE ON public.v_pool_maps FOR EACH ROW EXECUTE FUNCTION public.td_v_pool_maps(); 
