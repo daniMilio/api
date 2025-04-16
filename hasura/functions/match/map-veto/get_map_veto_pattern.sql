3dadbce879be72586863c5051c95767a7bacf8bd
Luke Policinski <Luke@LukePOLO.com>
2025-04-15 20:59:09 -0400

    bug: fix issue with veto process and too few maps (#44)

--- hasura/functions/match/map-veto/get_map_veto_pattern.sql
+++ hasura/functions/match/map-veto/get_map_veto_pattern.sql
@@ -5,56 +5,44 @@ DECLARE
     pool uuid[];
 	best_of int;
     pattern TEXT[] := '{}';
-    base_pattern TEXT[] := ARRAY['Ban', 'Ban', 'Pick', 'Pick'];
-    picks_count INT;
-    picks_left INT;
+    base_pattern TEXT[] := '{}';
     pattern_length INT;
     i INT;
+    pool_size INT;
+    _type TEXT;
+    base_pattern_length INT;
 BEGIN
 	SELECT mo.best_of INTO best_of
         FROM matches m
         INNER JOIN match_options mo on mo.id = m.match_options_id
         where m.id = _match.id;
+
     SELECT array_agg(mp.map_id) INTO pool
         FROM matches m
         INNER JOIN match_options mo on mo.id = m.match_options_id
         LEFT JOIN _map_pool mp ON mp.map_pool_id = mo.map_pool_id
-        LEFT JOIN match_map_veto_picks mvp ON mvp.match_id = _match.id AND mvp.map_id = mp.map_id
         WHERE m.id = _match.id;
-    -- Loop to build the pattern array
-    WHILE array_length(pattern, 1) IS DISTINCT FROM coalesce(array_length(pool, 1), 0) - 1 LOOP
-        -- Count the number of 'Pick' elements in the pattern array
-        picks_count := 0;
-        IF array_length(pattern, 1) IS NOT NULL THEN
-            FOR i IN 1..array_length(pattern, 1) LOOP
-                IF pattern[i] = 'Pick' THEN
-                    picks_count := picks_count + 1;
-                END IF;
-            END LOOP;
-        END IF;
-        -- Logic for adding elements to the pattern array
-        IF picks_count = best_of - 1 THEN
-            pattern := array_append(pattern, 'Ban');
-            CONTINUE;
-        END IF;
-        picks_left := coalesce(array_length(pool, 1), 0) - coalesce(array_length(pattern, 1), 0) - 1;
-        IF picks_left < picks_count + 2 THEN
-            pattern := array_append(pattern, 'Pick');
-            CONTINUE;
-        END IF;
-        pattern := pattern || base_pattern[1:picks_left];
+
+    pool_size := coalesce(array_length(pool, 1), 0);
+    
+    IF pool_size < best_of + 2 THEN
+        base_pattern := ARRAY['Ban', 'Pick', 'Pick'];
+    ELSE
+        base_pattern := ARRAY['Ban', 'Ban', 'Pick', 'Pick'];
+    END IF;
+
+    FOR i IN 1..(pool_size - 1) LOOP
+        _type := base_pattern[1 + ((i - 1) % array_length(base_pattern, 1))];
+        
+        pattern := array_append(pattern, _type);
+        
+        if _type = 'Pick' then
+            pattern := array_append(pattern, 'Side');
+        end if;
     END LOOP;
-    -- Insert 'Side' elements after each 'Pick' in the pattern array
-    pattern_length := coalesce(array_length(pattern, 1), 0);
-    i := 1;
-    WHILE i <= pattern_length LOOP
-        IF pattern[i] = 'Pick' THEN
-            pattern := array_cat(array_cat(pattern[1:i], ARRAY['Side']), pattern[i+1:pattern_length]);
-            pattern_length := pattern_length + 1;
-            i := i + 1; -- Skip the next element as it is newly added
-        END IF;
-        i := i + 1;
-    END LOOP;
+
+    pattern := array_append(pattern, 'Decider');
+    
     RETURN pattern;
 END;
 $$;
--- hasura/triggers/matches.sql
+++ hasura/triggers/matches.sql
@@ -51,30 +51,37 @@ CREATE OR REPLACE FUNCTION public.tai_match() RETURNS TRIGGER
 DECLARE
     _map_id UUID;
     _map_pool_id UUID;
+    _map_pool UUID[];
+    _map_pool_count int;
     _best_of int;
     _max_players_per_lineup int;
     available_regions text[];
-    map_pool_count int;
     _lobby_id UUID;
     lobby_players bigint[];
 BEGIN
     SELECT map_pool_id, best_of INTO _map_pool_id, _best_of FROM match_options WHERE id = NEW.match_options_id;
 
-    SELECT COUNT(*) INTO map_pool_count FROM _map_pool WHERE map_pool_id = _map_pool_id;
+    SELECT array_agg(map_id) INTO _map_pool FROM _map_pool WHERE map_pool_id = _map_pool_id;
 
-    IF map_pool_count = 0 THEN
+    _map_pool_count = array_length(_map_pool, 1);
+
+    IF _map_pool_count = 0 THEN
         RAISE EXCEPTION USING ERRCODE = '22000', MESSAGE = 'Match requires at least one map selected';
     END IF;
 
-    IF _best_of > map_pool_count THEN
+    IF _best_of > _map_pool_count THEN
         RAISE EXCEPTION USING ERRCODE = '22000', MESSAGE = 'Not enough maps in the pool for the best of ' || _best_of;
     END IF;
 
     SELECT map_id INTO _map_id FROM _map_pool WHERE map_pool_id = _map_pool_id LIMIT 1;
 
-    IF map_pool_count = 1 THEN 
-        INSERT INTO match_maps (match_id, map_id, "order", lineup_1_side, lineup_2_side)
-            VALUES (NEW.id, _map_id, 1, 'CT', 'TERRORIST');
+    IF _map_pool_count = _best_of THEN 
+        FOR i IN 1.._best_of LOOP
+            INSERT INTO match_maps (match_id, map_id, "order", lineup_1_side, lineup_2_side)
+            VALUES (NEW.id, _map_pool[i], i, 
+                    CASE WHEN i % 2 = 1 THEN 'CT' ELSE 'TERRORIST' END, 
+                    CASE WHEN i % 2 = 1 THEN 'TERRORIST' ELSE 'CT' END);
+        END LOOP;
     END IF;
 
    IF NOT is_tournament_match(NEW) THEN
