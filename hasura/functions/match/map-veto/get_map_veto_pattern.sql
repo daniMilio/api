d27217f5ff87e4cb716b105cc4f4206a256d15b7
Luke Policinski <Luke@LukePOLO.com>
2025-04-19 05:21:04 -0400

    Update get_map_veto_pattern.sql (#47)

--- hasura/functions/match/map-veto/get_map_veto_pattern.sql
+++ hasura/functions/match/map-veto/get_map_veto_pattern.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE FUNCTION public.get_map_veto_pattern(_match public.matches) RETURNS text[]
     LANGUAGE plpgsql
-    AS $$
+AS $$
 DECLARE
     pool uuid[];
-	best_of int;
+    best_of int;
     pattern TEXT[] := '{}';
     base_pattern TEXT[] := '{}';
     pattern_length INT;
@@ -12,20 +12,22 @@ DECLARE
     _type TEXT;
     base_pattern_length INT;
 BEGIN
-	SELECT mo.best_of INTO best_of
-        FROM matches m
-        INNER JOIN match_options mo on mo.id = m.match_options_id
-        where m.id = _match.id;
+    SELECT mo.best_of INTO best_of
+    FROM matches m
+    INNER JOIN match_options mo ON mo.id = m.match_options_id
+    WHERE m.id = _match.id;
 
     SELECT array_agg(mp.map_id) INTO pool
-        FROM matches m
-        INNER JOIN match_options mo on mo.id = m.match_options_id
-        LEFT JOIN _map_pool mp ON mp.map_pool_id = mo.map_pool_id
-        WHERE m.id = _match.id;
+    FROM matches m
+    INNER JOIN match_options mo ON mo.id = m.match_options_id
+    LEFT JOIN _map_pool mp ON mp.map_pool_id = mo.map_pool_id
+    WHERE m.id = _match.id;
 
     pool_size := coalesce(array_length(pool, 1), 0);
-    
-    IF pool_size < best_of + 2 THEN
+
+    IF best_of = 1 THEN
+        base_pattern := ARRAY['Ban'];
+    ELSIF pool_size < best_of + 2 THEN
         base_pattern := ARRAY['Ban', 'Pick', 'Pick'];
     ELSE
         base_pattern := ARRAY['Ban', 'Ban', 'Pick', 'Pick'];
@@ -33,16 +35,15 @@ BEGIN
 
     FOR i IN 1..(pool_size - 1) LOOP
         _type := base_pattern[1 + ((i - 1) % array_length(base_pattern, 1))];
-        
         pattern := array_append(pattern, _type);
-        
-        if _type = 'Pick' then
+
+        IF _type = 'Pick' THEN
             pattern := array_append(pattern, 'Side');
-        end if;
+        END IF;
     END LOOP;
 
     pattern := array_append(pattern, 'Decider');
-    
+
     RETURN pattern;
 END;
 $$;
