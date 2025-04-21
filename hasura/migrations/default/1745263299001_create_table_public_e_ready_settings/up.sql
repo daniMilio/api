aec09717285e78ac832c527c2f331d026d57d9fe
Luke Policinski <Luke@LukePOLO.com>
2025-04-21 18:07:34 -0400

    bug: migration fix

--- hasura/migrations/default/1745263299001_create_table_public_e_ready_settings/up.sql
+++ hasura/migrations/default/1745263299001_create_table_public_e_ready_settings/up.sql
@@ -1,5 +1,12 @@
 CREATE TABLE "public"."e_ready_settings" ("value" text NOT NULL, "description" text NOT NULL, PRIMARY KEY ("value") );
 
+insert into e_ready_settings ("value", "description") values
+    ('Players', 'All Players'),
+    ('Captains', 'Captains Only'),
+    ('Coach', 'Coach Only'),
+    ('Admin', 'Admins Only')
+on conflict(value) do update set "description" = EXCLUDED."description"
+ 
 alter table "public"."match_options" add column "ready_setting" text
  not null default 'Players';
 
